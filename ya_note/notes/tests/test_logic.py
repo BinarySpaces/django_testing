9bb2dcffaa38477125a7bf10d8c64129aa9d1b0e
Andrey Starikov <flashbum2003@yandex.ru>
2025-02-26 14:07:46 +0300

    updates

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -31,27 +31,27 @@ class TestNoteLogic(TestBaseClass):
             set(Note.objects.values_list('id', flat=True))
         )
 
-    def create_note_and_assert(self, form_data):
+    def create_note_and_assert(self, expect_slug_generation=True):
         Note.objects.all().delete()
-        response = self.auth_author.post(NOTES_ADD_URL, data=form_data)
+        response = self.auth_author.post(NOTES_ADD_URL, data=self.form_data)
         self.assertRedirects(response, NOTES_SUCCESS_URL)
         self.assertEqual(Note.objects.count(), 1)
-        return Note.objects.get()
+        note = Note.objects.get()
+        self.assertEqual(note.title, self.form_data['title'])
+        self.assertEqual(note.text, self.form_data['text'])
+        self.assertEqual(note.author, self.note.author)
+        if expect_slug_generation:
+            self.assertEqual(note.slug, slugify(self.form_data['title']))
+        else:
+            self.assertEqual(note.slug, slugify(self.form_data['slug']))
+        return note
 
     def test_slug_auto_generation_if_not_provided(self):
         self.form_data.pop('slug')
-        note = self.create_note_and_assert(self.form_data)
-        self.assertEqual(note.title, self.form_data['title'])
-        self.assertEqual(note.text, self.form_data['text'])
-        self.assertEqual(note.slug, slugify(self.form_data['title']))
-        self.assertEqual(note.author, self.author)
+        self.create_note_and_assert()
 
     def test_auth_user_can_create_note(self):
-        note = self.create_note_and_assert(self.form_data)
-        self.assertEqual(note.title, self.form_data['title'])
-        self.assertEqual(note.text, self.form_data['text'])
-        self.assertEqual(note.slug, self.form_data['slug'])
-        self.assertEqual(note.author, self.author)
+        self.create_note_and_assert(expect_slug_generation=False)
 
     def test_anonymous_user_cant_create_note(self):
         before_response_notes = set(Note.objects.values_list('id', flat=True))
@@ -67,38 +67,37 @@ class TestNoteLogic(TestBaseClass):
         response = self.auth_author.delete(DELETE_SLUG_URL)
         self.assertRedirects(response, NOTES_SUCCESS_URL)
         self.assertEqual(Note.objects.count(), note_count - 1)
-        self.assertFalse(Note.objects.filter(pk=self.note.pk).exists())
+        self.assertFalse(Note.objects.filter(id=self.note.id).exists())
 
-    def test_other_user_cant_delete_note(self):
-        response = self.auth_other_user.post(DELETE_SLUG_URL)
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.assertTrue(Note.objects.filter(pk=self.note.pk).exists())
-
-        note = Note.objects.get(pk=self.note.pk)
-        self.assertEqual(note.title, self.note.title)
-        self.assertEqual(note.text, self.note.text)
-        self.assertEqual(note.slug, self.note.slug)
-        self.assertEqual(note.author, self.note.author)
+    def get_note(self):
+        return Note.objects.get(id=self.note.id)
 
     def test_author_can_edit_note(self):
         response = self.auth_author.post(EDIT_SLUG_URL, data=self.form_data)
         self.assertRedirects(response, NOTES_SUCCESS_URL)
 
-        note = Note.objects.get(pk=self.note.pk)
+        note = self.get_note()
         self.assertEqual(note.text, self.form_data['text'])
         self.assertEqual(note.slug, self.form_data['slug'])
         self.assertEqual(note.title, self.form_data['title'])
         self.assertEqual(note.author, self.note.author)
 
-    def test_unauthorized_user_cannot_edit_note(self):
+    def assert_note_attributes(self, note):
+        self.assertEqual(note.text, self.note.text)
+        self.assertEqual(note.title, self.note.title)
+        self.assertEqual(note.slug, self.note.slug)
+        self.assertEqual(note.author, self.note.author)
+
+    def test_non_author_cant_delete_note(self):
+        response = self.auth_other_user.post(DELETE_SLUG_URL)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertTrue(Note.objects.filter(id=self.note.id).exists())
+        self.assert_note_attributes(self.get_note())
+
+    def test_non_author_cant_edit_note(self):
         response = self.auth_other_user.post(
             EDIT_SLUG_URL,
             data=self.form_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-
-        note = Note.objects.get(id=self.note.id)
-        self.assertEqual(self.note.text, note.text)
-        self.assertEqual(self.note.title, note.title)
-        self.assertEqual(self.note.slug, note.slug)
-        self.assertEqual(self.note.author, note.author)
+        self.assert_note_attributes(self.get_note())
